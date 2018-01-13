6cb567e53a754c37427372787cdd0c85898e3a25
Jakub Nowicki <j.nowicki@student.uw.edu.pl>
2018-01-13 18:50:39 +0100

    angle added

--- R/deform_set.R
+++ R/deform_set.R
@@ -9,13 +9,17 @@
 #'
 #' @export
 #'
-deform.set <- function(data, a.interval, theta.interval,noise.a.sd = 0.01 ,noise.theta.sd = 0.01, deform.set.seed = NA) {
+deform.set <- function(data, a.interval, theta.interval,noise.a.sd = 0.01 ,noise.theta.sd = 0.01, deform.set.seed = NA, angle.interval = NULL, nondeformed = 0) {
         if (class(data)=='list') {
                 landmarks <- data$landmarks
         } else {
                 landmarks <- data
         }
         n.specimens <- dim(landmarks)[3]
+        if(nondeformed > 0 & nondeformed < 1) {
+                nondeformed <- round(n.specimens*nondeformed, digits = 0)
+        }
+        nondeformed.specimens <- sample(1:n.specimens, size = nondeformed)
         output <- array(dim = dim(landmarks))
         if (!is.na(deform.set.seed)) {
                 set.seed(deform.set.seed)
@@ -26,18 +30,27 @@ deform.set <- function(data, a.interval, theta.interval,noise.a.sd = 0.01 ,noise
                 seed.vector <- sample(1:100000, size = n.specimens)
         }
         for (i in 1:n.specimens) {
-                if (!is.na(deform.set.seed)) {
-                        set.seed(seed = seed.matrix[i,1])
+                if (i %in% nondeformed.specimens) {
+                        output[,,i] <- landmarks[,,i]
+                } else {
+                        if (!is.na(deform.set.seed)) {
+                                set.seed(seed = seed.matrix[i,1])
+                        }
+                        a.tmp <- runif(n = 1, min = a.interval[1],max = a.interval[2])
+                        if (!is.na(deform.set.seed)) {
+                                set.seed(seed = seed.matrix[i,2])
+                        }
+                        theta.tmp <- runif(n = 1,min = theta.interval[1],max = theta.interval[2])
+                        if (!is.null(angle.interval)) {
+                                angle <- runif(n = 1, min = angle.interval[1], max = angle.interval[2])
+                        } else {
+                                angle <- NULL
+                        }
+                        ifelse(!is.na(deform.set.seed), yes = deform.seed <- NA, no = deform.seed <- seed.vector[i])
+                        output[,,i] <- deform.with.noise(data = landmarks[,,i],a = a.tmp,theta = theta.tmp,
+                                                         noise.a.sd = noise.a.sd,noise.theta.sd = noise.theta.sd,
+                                                         deform.seed = deform.seed, angle = angle)
                 }
-                a.tmp <- runif(n = 1, min = a.interval[1],max = a.interval[2])
-                if (!is.na(deform.set.seed)) {
-                        set.seed(seed = seed.matrix[i,2])
-                }
-                theta.tmp <- runif(n = 1,min = theta.interval[1],max = theta.interval[2])
-                ifelse(!is.na(deform.set.seed), yes = deform.seed <- NA, no = deform.seed <- seed.vector[i])
-                output[,,i] <- deform.with.noise(data = landmarks[,,i],a = a.tmp,theta = theta.tmp,
-                                                 noise.a.sd = noise.a.sd,noise.theta.sd = noise.theta.sd,
-                                                 deform.seed = deform.seed)
         }
         return(output)
 }
--- R/deform_with_noise.R
+++ R/deform_with_noise.R
@@ -6,16 +6,22 @@
 #' @param noise.a.sd standard deviation of a
 #' @param noise.theta.sd standard deviation of theta
 #' @param deform.seed deformation seed
+#' @param angle angle of deformation
 #'
 #' @import morphoutils
 #' @export
 #'
-deform.with.noise <- function(data,a,theta,noise.a.sd = 0.01 ,noise.theta.sd = 0.01, deform.seed = NA) {
+deform.with.noise <- function(data,a,theta,noise.a.sd = 0.01 ,noise.theta.sd = 0.01, deform.seed = NA, angle = NULL) {
         n.landmarks <- nrow(data)
         output <- matrix(0,nrow = n.landmarks,ncol = 2)
         if (!is.na(deform.seed)) {
                 set.seed(deform.seed)
                 seed.matrix <- matrix(sample(1:100000,size = 2*n.landmarks),ncol = 2)
+        }
+        if (!is.null(angle)) {
+                angle.matrix <- matrix(data = c(cos(angle), sin(angle),-sin(angle),cos(angle)), nrow = 2,ncol = 2)
+                data <- angle.matrix %*% t(data)
+                data <- t(data)
         }
         for (i in 1:n.landmarks) {
                 if (!is.na(deform.seed)) {
